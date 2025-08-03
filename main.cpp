2c103ffe4211cdc402c3e2976763d2629d106b4d
Joshua <37853883+esqa@users.noreply.github.com>
2025-08-04 00:22:11 +0200

    testing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
-__declspec(naked) int run_calculation(int a, int b)
+__declspec(naked) int run_calculation()
 {
     __asm
     {
         push ebp;
         mov ebp, esp;
+
+        mov eax, [ebp + 16];
+        mov edx, [ebp + 20];
 
         sub eax, edx;
         shr eax, 31;
@@ -16,25 +19,43 @@ __declspec(naked) int run_calculation(int a, int b)
     }
 }
 
+int case_0_func()
+{
+    return 10;
+}
+
+int case_1_func()
+{
+    return 50;
+}
+
+int (*jump_table[2])() = { case_0_func, case_1_func };
+
 int main()
 {
     int retval{};
 
     __asm
     {
-        mov eax, 10;
-        mov edx, 10;
+        push ebp;
+        mov ebp, esp;
+
+        push 40;
+        push 40;
+        push 30;
+        push 30;
+
         call run_calculation;
+
+        mov ecx, offset jump_table;
+        call dword ptr [ecx + eax * 4];
+
+        add esp, 16;
+
         mov retval, eax;
-
-        call get_eip;
-        get_eip:
-        pop ecx;
-
-        mov ecx, jmp_table;
-        jmp [jmp_table + retval * 4];
-
-        jmp_table:
+        mov esp, ebp;
+        pop ebp;
+        ret;
     }
 
     printf("we're here! %i", retval);
